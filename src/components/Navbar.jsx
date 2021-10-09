d25f7c009902793ed4f460650d77ae5612549a7b
unknown <codewithsrobins1@gmail.com>
2021-10-09 13:43:13 -0700

    testing a commit error

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { Button, Menu, Typography, Avatar } from 'antd';
 
 const Navbar = () => {
+
+    const stringy = "something"
     return (
         <div>
-            
+        
         </div>
     )
 }
